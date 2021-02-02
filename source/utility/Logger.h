7753748bba5c6d994f5a944bdb5691a984a2e794
Scott Gudeman <24697493+DragonCrafted87@users.noreply.github.com>
2021-02-02 02:09:32 +0000

    add new logger interface and implementation for basic rgb printouts

--- source/utility/Logger.h
+++ source/utility/Logger.h
@@ -4,11 +4,22 @@
 
 struct ILogger
 {
-	virtual void Log(std::string string) = 0;
+    virtual void Log(std::string string) = 0;
 };
 
-class Logger : public ILogger
+class IColorLogger : public ILogger
 {
 public:
-	void Log(std::string string);
+    virtual void LogRed(std::string string) = 0;
+    virtual void LogGreen(std::string string) = 0;
+    virtual void LogBlue(std::string string) = 0;
+};
+
+class Logger : public IColorLogger
+{
+public:
+    void Log(std::string string);
+    void LogRed(std::string string);
+    void LogGreen(std::string string);
+    void LogBlue(std::string string);
 };
