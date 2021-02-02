d0939de2d9fde72e641d3947fd64c5785ce41d2c
Shom Bandopadhaya <shom@belcan.com>
2021-02-02 11:17:51 -0600

    Add support for blue and red log output

--- source/Logger.cpp
+++ source/Logger.cpp
@@ -1,8 +1,19 @@
 #include "Logger.h"
 
+#include "../lib/termcolor.hpp"
 #include <iostream>
 
 void Logger::Log(std::string string)
 {
 	std::cout << string << std::endl;
+}
+void BlueLogger::Log(std::string string)
+{
+	std::cout << termcolor::blue << string;
+	std::cout << termcolor::reset << "" << std::endl;
+}
+void RedLogger::Log(std::string string)
+{
+	std::cout << termcolor::red << string;
+	std::cout << termcolor::reset << "" << std::endl;
 }
